8b1d4c95c8142ffdb53e9759688e28052baf9622
Lousy_malayali <93783539+CaptainHippie@users.noreply.github.com>
2022-06-20 19:14:53 +0530

    Update web.php

--- basic_login_crud/routes/web.php
+++ basic_login_crud/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
-use App\Http\Controllers\CRUDcontroller;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ImageController;
 
@@ -15,8 +14,6 @@ Route::post('/regsubmit',[CustomAuthController::class,'register_click'])->name('
 Route::post('/loginsubmit',[CustomAuthController::class,'login_click'])->name('user-login');
 Route::get('/userhome',[CustomAuthController::class,'login_success'])->middleware('IsLoggedIn');
 Route::get('/logout',[CustomAuthController::class,'logout']);
-
-Route::resource('students', CRUDcontroller::class);
 
 Route::resource('admin', Admincontroller::class);
 
